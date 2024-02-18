410dc08e1556bd509f2ff33465f279e7b574c698
qq2575896094 <2575896094@qq.com>
2024-02-18 20:11:26 +0800

    fix(eslint): fix eslint config

---
+++ packages/crw-template/template/.eslintrc.js
@@ -0,0 +1,33 @@
+const { alias = {} } = require('./crw.config')()
+
+module.exports = {
+    root: true,
+    parserOptions: {
+        ecmaVersion: 11,
+        sourceType: 'module',
+        allowImportExportEverywhere: true,
+        ecmaFeatures: {
+            jsx: true,
+            globalReturn: true,
+        },
+    },
+    extends: ['eslint:recommended', 'crw'],
+    rules: {},
+    settings: {
+        'import/resolver': {
+            webpack: {
+                config: {
+                    resolve: {
+                        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+                        alias,
+                    },
+                },
+            },
+        },
+    },
+    env: {
+        node: true,
+        browser: true,
+        es6: true,
+    },
+}
